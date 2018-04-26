8603bed4cd33ecb022522446e007dee7edb25d11
Ran Tayeb <Tayebran@gmail.com>
2018-04-26 21:07:23 +0300

    min changes

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_io_1 = require("./server-io");
 let app = new server_io_1.ServerIo().getApp();
-//export {app}
+exports.app = app;
--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 
 import {ServerIo} from './server-io'
 
-
 let app = new ServerIo().getApp();
 
-//export {app}
+export {app}
